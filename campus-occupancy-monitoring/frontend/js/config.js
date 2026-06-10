cec609166e124fb217ce788c554ac4ed64d81869
Steven Franco <scfb120406@gmail.com>
2026-06-10 00:51:56 -0500

    URL de render

--- campus-occupancy-monitoring/frontend/js/config.js
+++ campus-occupancy-monitoring/frontend/js/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
     API_BASE_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
         ? 'http://localhost:5000/api'
-        : 'https://TU-BACKEND-EN-RENDER.onrender.com/api',
+        : 'https://pdi-final.onrender.com/api',
 };
